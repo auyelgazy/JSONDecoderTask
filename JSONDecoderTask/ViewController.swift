6a5d95386166a03bd6974f64e264579b3d54bae3
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-05-15 13:48:19 +0600

    Initial Commit

---
+++ JSONDecoderTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONDecoderTask
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
