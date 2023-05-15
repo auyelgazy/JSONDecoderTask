b5bfef74a2bd1d420ec6bd3e5454a8ab260b67ca
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-05-15 16:53:25 +0600

    Created model and parsed data.

--- JSONDecoderTask/ViewController.swift
+++ JSONDecoderTask/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = createUrl(with: "Black Lotus")
+        getData(url: url)
     }
 
+    private func createUrl(with cardName: String) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.magicthegathering.io"
+        components.path = "/v1/cards"
+        components.queryItems = [URLQueryItem(name: "name", value: cardName)]
+        return components.url
+    }
 
+    private func getData(url: URL?) {
+        guard let url = url else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+
+            if let error = error {
+                print(error)
+            }
+
+            guard let response = response as? HTTPURLResponse else {
+                print("No response")
+                return
+            }
+
+            guard let data = data else {
+                print("No data")
+                return
+            }
+
+            print("[responseStatusCode] = \(String(describing: response.statusCode))\n")
+
+            do {
+                let jsonData = try JSONDecoder().decode(Cards.self, from: data)
+                guard let card = jsonData.cards.first else { return }
+                self.printCardInfo(card: card)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+
+    private func printCardInfo(card: Card) {
+        print("""
+              [data] = Card: \(card.name)
+              Manacost: \(card.manaCost ?? "No manacost")
+              CMC: \(card.cmc)
+              Type: \(card.type)
+              Rarity: \(card.rarity)
+              Printings: \(card.printings)
+              Original Text: \(card.originalText ?? "This card has no original text.")
+              """)
+    }
 }
-
